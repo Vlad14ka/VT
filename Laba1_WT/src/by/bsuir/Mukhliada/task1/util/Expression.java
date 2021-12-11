1fdfee3b6f2e689537730c5d837225dfbcc398da
Vlad Pomelnikov <71628133+Vlad14ka@users.noreply.github.com>
2021-12-11 23:30:47 +0300

    Delete Laba1_WT directory

--- Laba1_WT/src/by/bsuir/Mukhliada/task1/util/Expression.java
+++
@@ -1,7 +0,0 @@
-package by.bsuir.Mukhliada.task1.util;
-
-public class Expression {
-    public static double solve(double x, double y) {
-        return (1 + Math.pow(Math.sin(x + y), 2))/(2 + Math.abs(x - (2 * x)/(1 + x*x * y*y))) + x;
-    }
-}
